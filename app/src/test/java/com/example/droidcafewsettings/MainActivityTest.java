a92a1859f4422f9365c03501bbe8ae16896503e6
prudhvi <prudhvi.satram1995@gmail.com>
2019-09-18 17:57:35 -0400

    getting the sharedpreferences and setting,comparing values tried

--- app/src/test/java/com/example/droidcafewsettings/MainActivityTest.java
+++ app/src/test/java/com/example/droidcafewsettings/MainActivityTest.java
@@ -1,6 +1,8 @@
 package com.example.droidcafewsettings;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.widget.AppCompatImageView;
 import android.view.Menu;
 import android.widget.Button;
@@ -10,6 +12,7 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertThat;
+import static org.robolectric.Shadows.shadowOf;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,14 +20,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadow.api.Shadow;
 import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowIntent;
 import org.robolectric.shadows.ShadowToast;
 
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 @RunWith(RobolectricTestRunner.class)
 //@Config(constants = BuildConfig.class)
@@ -33,6 +39,12 @@ import android.widget.ImageView;
 public class MainActivityTest {
 
     private MainActivity activity;
+    private final static String FILENAME = "filename";
+    private SharedPreferences.Editor editor;
+    private SharedPreferences sharedPreferences;
+
+    private Context context;
+    private String etName;
 
     @Before
     public void setUp() throws Exception {
@@ -40,6 +52,13 @@ public class MainActivityTest {
                 .create()
                 .resume()
                 .get();
+
+
+        context = ApplicationProvider.getApplicationContext();
+
+        sharedPreferences = context.getSharedPreferences(FILENAME, Context.MODE_PRIVATE);
+        // Ensure no shared preferences have leaked from previous tests.
+        assertThat(sharedPreferences.getAll()).hasSize(0);
     }
 
     @Test
@@ -49,11 +68,48 @@ public class MainActivityTest {
 
     @Test
     public void testButtonClickShouldShowToast() throws Exception {
+        //1.get the button id
+        //2click the button
+        //3.check the toast
+        //4.compare it with expected value
+
+
         MainActivity activity = Robolectric.buildActivity(MainActivity.class).create().get();
+        //get the id of image view
         ImageView view = (ImageView) activity.findViewById(R.id.froyo);
+        //check the view is not null
         assertNotNull(view);
+        //click the image view
         view.performClick();
+        //comparing actual vs expected
         assertThat(ShadowToast.getTextOfLatestToast(), equalTo("You ordered a FroYo.") );
+    }
+
+
+    @Test
+    public void cartPageRemembersUSerInfo() throws Exception {
+
+//
+     //   Test that the app remembers the information the user entered on the Shopping Cart page.
+        //1.test the sharedpreferences are empty
+
+        //2.add some data
+        //3.check the data entered equals
+        //4.close the app and enter the order activity again
+        //5.check the data is still equals the last entered data
+
+        editor.commit();
+
+      SharedPreferences anotherSharedPreferences = context.getSharedPreferences(FILENAME, Context.MODE_PRIVATE);
+           assertTrue(anotherSharedPreferences.getString("string", "prudhvi")).isEqualTo(etName.getText().toString());
+
+
+           SharedPreferences sharedPreferences = Robolectric.application.getSharedPreferences("you_custom_pref_name", Context.MODE_PRIVATE);
+           sharedPreferences.edit().putString("name", "prudhvi").commit();
+        assertThat(Shadow(etName).getText().toString(), equalTo("prudhvi") );
+
+
+
     }
 
 
